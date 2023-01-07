c403198e87dfa93d9b68d14cacdea6c576d02a0c
xMimilou <Louka06.millon@gmail.com>
2023-01-07 15:56:25 +0100

    adding vuetify

--- microservice_commercial/src/main.ts
+++ microservice_commercial/src/main.ts
@@ -3,12 +3,22 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
 import './assets/main.css'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+  })
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 
-app.mount('#app')
+app.use(vuetify).mount('#app')
