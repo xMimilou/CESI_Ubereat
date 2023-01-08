c43c9a2af185d8ac8c37ec504bbc6cdb01350d20
xMimilou <Louka06.millon@gmail.com>
2023-01-08 18:19:44 +0100

    design setup

--- microservice_commercial/src/App.vue
+++ microservice_commercial/src/App.vue
@@ -5,81 +5,65 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
-    </div>
+      <nav class="end">
+        <RouterLink to="/login">Login</RouterLink>
+        <RouterLink to="/register">Register</RouterLink>
+      </nav>
   </header>
-
   <RouterView />
 </template>
 
 <style scoped>
 header {
   line-height: 1.5;
-  max-height: 100vh;
+  height: 50px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
 nav {
   width: 100%;
   font-size: 12px;
   text-align: center;
-  margin-top: 2rem;
+  display: flex;
 }
-
+nav.end{
+  justify-content: flex-end;
+}
 nav a.router-link-exact-active {
   color: var(--color-text);
 }
-
 nav a.router-link-exact-active:hover {
   background-color: transparent;
 }
-
 nav a {
   display: inline-block;
   padding: 0 1rem;
   border-left: 1px solid var(--color-border);
 }
-
 nav a:first-of-type {
   border: 0;
 }
-
 @media (min-width: 1024px) {
   header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+    float: left;
+    width: 100%;
   }
-
   .logo {
     margin: 0 2rem 0 0;
   }
-
   header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+    width: 100%;
   }
-
   nav {
     text-align: left;
-    margin-left: -1rem;
     font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
+    padding: 1rem 0rem;
   }
 }
+ /* Transition appear text from bottom */
 </style>
--- microservice_commercial/src/components/HelloWorld.vue
+++ microservice_commercial/src/components/HelloWorld.vue
@@ -7,34 +7,48 @@ defineProps<{
 <template>
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
-    </h3>
   </div>
 </template>
 
 <style scoped>
 h1 {
   font-weight: 500;
-  font-size: 2.6rem;
   top: -10px;
 }
-
 h3 {
   font-size: 1.2rem;
 }
-
+/* align item verticaly center */
+.greetings {
+  z-index: -1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 40vh;
+  font-size: 50px;
+  transform: translateY(150px);
+  animation: fadein 2s;
+}
+  /* Transition appear text from bottom */
+  @keyframes fadein {
+    from {
+      opacity: 0;
+      transform: translate3d(0, 150%, 0);
+    }
+    to {
+      opacity: 1;
+      transform: translate3d(0, 150px, 0);
+    }
+  }
 .greetings h1,
 .greetings h3 {
   text-align: center;
 }
-
 @media (min-width: 1024px) {
   .greetings h1,
   .greetings h3 {
-    text-align: left;
+    text-align: center;
   }
 }
 </style>
