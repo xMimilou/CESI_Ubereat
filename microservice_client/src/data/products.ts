db0eb100905bca9698dc0e5ed0e63df3beff41bc
xMimilou <Louka06.millon@gmail.com>
2023-01-19 16:30:18 +0100

    correctif

--- microservice_client/src/data/products.ts
+++ microservice_client/src/data/products.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export async function fetchRestaurants() {
   try {
-    const response = await axios.get("http://localhost/restorer/restaurants");
+    const response = await axios.get("http://localhost:3002/restorer/restaurants");
     let restaurants = response.data;
     return restaurants;
   } catch (error) {
